e69c47272206c915b9ca7b44fe250c5d2242593c
João_Azevedo <jazevedo960@gmail.com>
2019-04-19 09:55:30 +0100

    updates no ag

--- src/agregador_src/ag.c
+++ src/agregador_src/ag.c
@@ -4,39 +4,53 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <glib.h>
+#include <stdlib.h>
 
 #include "global.h"
 
 int main () {
 
-	GHashTable *file_lines = NULL;
+	if (system("clear")!=-1);
 
-	file_lines = g_hash_table_new(g_str_hash, g_str_equal); 
+	GHashTable *tableLines = NULL;
+
+	tableLines = g_hash_table_new(g_str_hash, g_str_equal); 
 
  	int fd_vendas = open(PATH_VENDAS, O_RDONLY, 0666);
 
+ 	int n = 1;
 
- 	int n = 0;
+ 	char key[MAX_LINE];
+ 	char value[MAX_LINE];
  	char buffer[MAX_LINE];
+ 
+ 	gboolean existeYet;
+	
+	n = readln(fd_vendas, buffer, MAX_LINE);
+ 
  	do {
 
+ 		sscanf(buffer, "%s %s", key, value);
+
+ 		existeYet = g_hash_table_insert (tableLines, 
+ 							 			strdup(key), 
+ 							 			strdup(buffer));
+
  		n = readln(fd_vendas, buffer, MAX_LINE);
-
- 		gpointer codigo, quantidade, montante;
- 
- 		sscanf(buffer, "%d %d %lf", &codigo, &quantidade, &montante);
-
- 		g_hash_table_insert (file_lines, strdup(buffer), codigo);
-
+ 	
  	} while (n > 0);
 
- 	GList *l, *keys;
+	GList *l, 
+		  *keys = g_hash_table_get_keys(tableLines),
+		  *values = g_hash_table_get_values(tableLines);
 
- 	keys = g_hash_table_get_keys(file_lines);
+	for (l = keys; l != NULL; l = l -> next) {
+		printf("key=%s\n", (char*) l -> data);
+	}
 
- 	for (l = keys; l != NULL; l = l -> next) {
- 		printf("%s", (char*) l->data);
- 	}
+	for (l = values; l != NULL; l = l -> next) {
+		printf("val=%s", (char*) l -> data);
+	}
 
 	close(fd_vendas);
 
