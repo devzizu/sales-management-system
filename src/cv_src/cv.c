c7f45d4de3f2aebef65008f578021019960d4bce
João_Azevedo <jazevedo960@gmail.com>
2019-05-05 15:52:47 +0100

    agregador no ma done

--- src/cv_src/cv.c
+++ src/cv_src/cv.c
@@ -112,109 +112,90 @@ int main(int argc, char* argv[]) {
 		if(clientRequest[strlen(clientRequest)-1] == '\n')
 			clientRequest[strlen(clientRequest)-1]='\0';
 
-		if (!strcmp(clientRequest, AGREGADOR)) {
+		spaces = nr_spaces_in_string(clientRequest);
 
-			pid = fork();
+		if (spaces == 0 && strlen(clientRequest) > 0) {
 
-			if (pid == 0) {
+			codigoPedido = is_number_int(clientRequest);
 
-				execl(AGR_EXEC_PATH, "./ag", NULL);
+			if (codigoPedido > 0) {
 
-				_exit(0);
-
-			} else if (pid > 0) {
-
-				wait(NULL);
-
-			}
-			
-		} else {
-
-			spaces = nr_spaces_in_string(clientRequest);
-
-			if (spaces == 0 && strlen(clientRequest) > 0) {
-
-				codigoPedido = is_number_int(clientRequest);
-
-				if (codigoPedido > 0) {
-
-					sprintf(pedidoBuffer, "%07d %010d %010d", processID, 
-												      	 	  codigoPedido,
-												      	 	  0);
-				} else {
-
-					if (write(errorLog, invalidCMD, strlen(invalidCMD)) != -1);
-
-					continue;
-				}
-
-			} else if (spaces == 1) {
-
-				successScanf = sscanf(clientRequest, "%d %d", &codigo, &quantidade);
-
-				if (successScanf == 2) {
-
-					sprintf(pedidoBuffer, "%07d %010d %010d", processID, 
-						                             	 	  codigo,
-						                              		  quantidade);
-				} else {
-
-					if (write(errorLog, invalidCMD, strlen(invalidCMD)) != -1);
-
-					continue;					
-				}
-
+				sprintf(pedidoBuffer, "%07d %010d %010d", processID, 
+											      	 	  codigoPedido,
+											      	 	  0);
 			} else {
-				
+
 				if (write(errorLog, invalidCMD, strlen(invalidCMD)) != -1);
-				
+
 				continue;
 			}
-			
-			//Enviar pedido para o servidor
-			if(write(pipeEnvioServer, pedidoBuffer, TAM_PEDIDO) != -1);
 
-			if (read(fd_rdwr, serverAnswer, TAM_RESPOSTA) == -1);
+		} else if (spaces == 1) {
 
-			sscanf(serverAnswer, "%d %d %d %d %d", &tipoDeResposta,
-													(int*) &clientAnswerID, 
-													&codigo,
-													&quantidade,
-													&precoLido);
-					
-			switch (tipoDeResposta) {
+			successScanf = sscanf(clientRequest, "%d %d", &codigo, &quantidade);
 
-				//Mostrar resultado de printStockPreco
-				case 0: 
-					sprintf(resposta, "Artigo: %d | Stock: %d | Preco: %d\n", codigo,
-																			  quantidade,
-																		      precoLido);
-					if (write(1 , resposta, strlen(resposta))!=-1);
-				
-					break;
+			if (successScanf == 2) {
 
-				case 1:
-					sprintf(resposta, "Quantidade de stock: %d\n", quantidade);
-					
-					if (write(1 , resposta, strlen(resposta))!=-1);
-				
-					break;
+				sprintf(pedidoBuffer, "%07d %010d %010d", processID, 
+					                             	 	  codigo,
+					                              		  quantidade);
+			} else {
 
-				case 3:
+				if (write(errorLog, invalidCMD, strlen(invalidCMD)) != -1);
 
-					if (write(errorLog, overflowCode, strlen(overflowCode)) != -1);
-
-					break;
-
-				case 4:
-
-					if (write(errorLog, errorUpdateStock, strlen(errorUpdateStock)) != -1);
-
-					break;
-
-				default: break;
-		
+				continue;					
 			}
+
+		} else {
+			
+			if (write(errorLog, invalidCMD, strlen(invalidCMD)) != -1);
+			
+			continue;
+		}
+		
+		//Enviar pedido para o servidor
+		if(write(pipeEnvioServer, pedidoBuffer, TAM_PEDIDO) != -1);
+
+		if (read(fd_rdwr, serverAnswer, TAM_RESPOSTA) == -1);
+
+		sscanf(serverAnswer, "%d %d %d %d %d", &tipoDeResposta,
+												(int*) &clientAnswerID, 
+												&codigo,
+												&quantidade,
+												&precoLido);
+				
+		switch (tipoDeResposta) {
+
+			//Mostrar resultado de printStockPreco
+			case 0: 
+				sprintf(resposta, "Artigo: %d | Stock: %d | Preco: %d\n", codigo,
+																		  quantidade,
+																	      precoLido);
+				if (write(1 , resposta, strlen(resposta))!=-1);
+			
+				break;
+
+			case 1:
+				sprintf(resposta, "Quantidade de stock: %d\n", quantidade);
+				
+				if (write(1 , resposta, strlen(resposta))!=-1);
+			
+				break;
+
+			case 3:
+
+				if (write(errorLog, overflowCode, strlen(overflowCode)) != -1);
+
+				break;
+
+			case 4:
+
+				if (write(errorLog, errorUpdateStock, strlen(errorUpdateStock)) != -1);
+
+				break;
+
+			default: break;
+		
 		}
 	} 
 	
--- src/ma_src/ma.c
+++ src/ma_src/ma.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+
 #include <fcntl.h>
 #include <unistd.h>
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "../GLOBAL_SOURCE/global.h"
+
+/**MACRO path base do pipe servidor >> cliente*/
+#define BASE_PATH "../PipeVendas/pipePrintCliente"
+/**MACRO path para o executavel do agregador*/
+#define AGR_EXEC_PATH "../ag_src/ag"
+/**MACRO nome do comando do agregador*/
+#define AGREGADOR "a"
 
 static char err_reading_num[24] = "[./ma] invalid number.\n";
 static char err_reading_cmd[25] = "[./ma] invalid command.\n";
@@ -23,8 +33,11 @@ void atualizaNome (int ref_antiga, char *novoNome) {
 
 	int fd_string = open(PATH_STRINGS, O_APPEND|O_WRONLY, 0666);
 
+	char nameBuffer[MAX_LINE];
+	sprintf(nameBuffer, "%s\n", novoNome);
+	
 	//Acrestar o novo nome ao fim, ja tem \n
-	if (write(fd_string, novoNome, strlen(novoNome)) != -1);
+	if (write(fd_string, nameBuffer, strlen(nameBuffer)) != -1);
 
 	//Já não preciso mais desse ficheiro, logo fecho-o
 	close(fd_string);
@@ -112,6 +125,8 @@ int main () {
 	//Numeros lidos apos parse de erros
 	int verified_int, verified_code;
 
+	pid_t pid;
+
 	//---------------------------------------------------------
 
 	while (n > 0) {
@@ -119,55 +134,80 @@ int main () {
 		//Ler o comando do stdin
 		n = readln(0, buffer, MAX_LINE);
 
-		if (nr_spaces_in_string(buffer) == 2) {
+		if(buffer[strlen(buffer)-1] == '\n')
+			buffer[strlen(buffer)-1]='\0';
 
-			//Separar o comando lido num array de strings
-			campos = tokenizeComando(buffer);
+		if (!strcmp(buffer, AGREGADOR)) {
 
-			//Controlo da função a executar mediante o comando
-			switch(campos[0][0]) {
+			pid = fork();
 
-				//Inserir novo artigo:> i <nome> <preco>
-				case 'i': 
+			if (pid == 0) {
 
-						//Caso corra bem esta função retorna > 0 (o número)
-						verified_int = is_number_int(campos[2]);
+				execl(AGR_EXEC_PATH, "./ag", NULL);
 
-						if (verified_int >= 0)
-							inserirArtigo(campos[1], verified_int, 0);
-						else
-							if (write(errorsLog, err_reading_num, strlen(err_reading_num)) != -1);
-						
+				_exit(0);
+
+			} else if (pid > 0) {
+
+				wait(NULL);
+
+			}
+			
+		} else {
+
+			if (nr_spaces_in_string(buffer) == 2) {
+
+				//Separar o comando lido num array de strings
+				campos = tokenizeComando(buffer);
+
+				//Controlo da função a executar mediante o comando
+				switch(campos[0][0]) {
+
+					//Inserir novo artigo:> i <nome> <preco>
+					case 'i': 
+
+							//Caso corra bem esta função retorna > 0 (o número)
+							verified_int = is_number_int(campos[2]);
+
+							if (verified_int >= 0)
+								inserirArtigo(campos[1], verified_int, 0);
+							else
+								if (write(errorsLog, err_reading_num, strlen(err_reading_num)) != -1);
+							
+							break;
+
+					//Alterar o nome do artigo:> n <codigo> <novo nome>
+					case 'n':
+							verified_int = is_number_int(campos[1]);
+
+							if (verified_int >= 0)
+								atualizaNome(verified_int, campos[2]);
+							else
+								if (write(errorsLog, err_reading_num, strlen(err_reading_num)) != -1);
+
+							break;
+
+					//Alterar o preco do artigo:> p <codigo> <novo preco>
+					case 'p':
+
+							verified_int  = is_number_int(campos[2]);
+							verified_code = is_number_int(campos[1]);
+
+							if (verified_int >= 0 && verified_code >= 0)
+								atualizaPreco(verified_code, verified_int);
+							else
+								if (write(errorsLog, err_reading_num, strlen(err_reading_num)) != -1);
+
+							break;
+
+					default: 
+
+							if (write(errorsLog, err_reading_cmd, strlen(err_reading_cmd)) != -1);
 						break;
+				}
+			} else {
 
-				//Alterar o nome do artigo:> n <codigo> <novo nome>
-				case 'n':
-						verified_int = is_number_int(campos[1]);
-
-						if (verified_int >= 0)
-							atualizaNome(verified_int, campos[2]);
-						else
-							if (write(errorsLog, err_reading_num, strlen(err_reading_num)) != -1);
-
-						break;
-
-				//Alterar o preco do artigo:> p <codigo> <novo preco>
-				case 'p':
-
-						verified_int  = is_number_int(campos[2]);
-						verified_code = is_number_int(campos[1]);
-
-						if (verified_int >= 0 && verified_code >= 0)
-							atualizaPreco(verified_code, verified_int);
-						else
-							if (write(errorsLog, err_reading_num, strlen(err_reading_num)) != -1);
-
-						break;
-
-				default: 
-
-						if (write(errorsLog, err_reading_cmd, strlen(err_reading_cmd)) != -1);
-					break;
+				if (write(errorsLog, err_reading_cmd, strlen(err_reading_cmd)) != -1);
 			}
 		}
 	}
